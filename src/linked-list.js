83216980d9c8f8d7380036747e892303b97ae8ea
Alina1989 <autoflame.by@gmail.com>
2017-01-31 21:08:18 +0200

    dd

--- src/linked-list.js
+++ src/linked-list.js
@@ -5,39 +5,40 @@ class LinkedList {
 
     constructor() {
     	this.length = 0;
-    	this.head = null;
-    	this.tail = null;
+    	this._head = null;
+    	this._tail = null;
     }
 
     append(data) {
 
-    	var node = new Node(data); current = this.tail
-    	if (this.length) {
-    	this.tail = node;
-        this.tail.prev = current;
-        this.tail.prev.next = this.tail; }
-    else {
-    	this.head = node;
-        this.tail = node; }
-
-this.length++;
-return this;
-    }
-
-    head() { if (this.head = data)
-    	return this.head
-    	else (this.head = null)
-    		return this.head
+    	        var node = new Node (data), current = this._tail;
+        if (this.length) {
+            this._tail = node;
+            this._tail.prev = current;
+            this._tail.prev.next = this._tail;
+        } else {
+            this._tail = node;
+            this._head = node;
+        }
+        this.length++;
+        return this;
 
     }
 
-    tail() { if (this.tail = data)
-    	return this.tail
-    	else (this.tail = null)
-    		return this.tail}
+    head() { if (this._head = data)
+    	return this._head
+    	else (this._head = null)
+    		return this._head
+
+    }
+
+    tail() { if (this._tail = data)
+    	return this._tail
+    	else (this._tail = null)
+    		return this._tail}
 
     at(index) { 
-     var current = this.head;
+     var current = this._head;
      while (current.data != index) {
      	if (current.next == null)
      		return null;
